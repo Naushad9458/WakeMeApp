5f35b992faee1478695bfe8c0b0da4e20b6c8230
Naushad Alam <ngc9458@gmail.com>
2017-03-20 02:08:03 +0530

    Added code for GPS on/off status

--- app/src/main/java/com/example/n5050/demoapp/GpsAlarm.java
+++ app/src/main/java/com/example/n5050/demoapp/GpsAlarm.java
@@ -1,13 +1,29 @@
 package com.example.n5050.demoapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
 
 public class GpsAlarm extends AppCompatActivity {
 
+    private int off;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps_alarm);
+        try{
+        off= Settings.Secure.getInt(getContentResolver(),Settings.Secure.LOCATION_MODE);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+        if(off==0){
+            Intent intent=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+
+        }
+
     }
 }

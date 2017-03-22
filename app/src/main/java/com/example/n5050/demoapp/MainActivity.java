e864e0aa5dd68a45d020bc626a08b2fe917895b2
Naushad Alam <ngc9458@gmail.com>
2017-03-22 12:05:34 +0530

    Modify Manifest.xml

--- app/src/main/java/com/example/n5050/demoapp/MainActivity.java
+++ app/src/main/java/com/example/n5050/demoapp/MainActivity.java
@@ -91,14 +91,6 @@ public class MainActivity extends AppCompatActivity
             startActivity(intent);
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);

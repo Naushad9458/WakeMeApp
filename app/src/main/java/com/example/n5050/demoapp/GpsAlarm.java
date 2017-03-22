ea2d97649ef8bc58234984959d1db90e5328840d
Naushad Alam <ngc9458@gmail.com>
2017-03-22 12:08:18 +0530

    Add an activity to handle gps Alarms

--- app/src/main/java/com/example/n5050/demoapp/GpsAlarm.java
+++ app/src/main/java/com/example/n5050/demoapp/GpsAlarm.java
@@ -1,18 +1,24 @@
 package com.example.n5050.demoapp;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
 
 public class GpsAlarm extends AppCompatActivity {
 
     private int off;
     Intent intent;
+    NotificationManager notificationManager;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,5 +53,33 @@ public class GpsAlarm extends AppCompatActivity {
             }
         });
         alertDialog.show();
+    }
+
+    public void startGPS(View view) {
+        Intent i=new Intent(this,FetchLocation.class);
+        startService(i);
+        showNotification();
+
+
+    }
+    public void showNotification(){
+        NotificationCompat.Builder notificationBuilder= (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setColor(ContextCompat.getColor(this,R.color.colorPrimary))
+                .setCategory(NotificationCompat.CATEGORY_SERVICE)
+                .setContentTitle("GPS Service Activated")
+                .setContentText("Running in Background")
+                .setDefaults(Notification.DEFAULT_SOUND|Notification.DEFAULT_VIBRATE)
+                .setSmallIcon(R.mipmap.icon)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText("GPS!"))
+                .setAutoCancel(true);
+
+        notificationManager=(NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(87123,notificationBuilder.build());
+
+    }
+
+    public void stopGps(View view) {
+        Intent intent=new Intent(this,FetchLocation.class);
+        stopService(intent);
     }
 }
